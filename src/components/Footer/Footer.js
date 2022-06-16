f6f313ec932c2abe95956a521637df5b0b64a2d3
ivanparmenas@gmail.com <ivanparmenas@gmail.com>
2022-06-17 02:22:25 +0700

    Route changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './Footer.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Footer.css";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faInstagram,
@@ -11,40 +11,43 @@ import {
 function Footer() {
   return (
     <div className="footer-container">
-    <section class="social-media">
-      <div class="social-media-wrap">
-        <small class="website-rights">
-          Made by Ivan Nathan Parmenas © 2022
-        </small>
-        <div class="social-icons">
-          <Link
-            class="social-icon-link instagram"
-            to={{ pathname: "https://www.instagram.com/ivan_nathan1/" }}
-            target="_blank"
-            aria-label="Instagram"
-          >
-            <FontAwesomeIcon icon={faInstagram} />
-          </Link>
-          <Link
-            class="social-icon-link whatsapp"
-            to={{ pathname: "https://wa.me/6289638303065" }}
-            target="_blank"
-            aria-label="Whatsapp"
-          >
-            <FontAwesomeIcon icon={faWhatsapp} />
-          </Link>
-          <Link
-            class="social-icon-link linkedin"
-            to={{ pathname: "www.linkedin.com/in/ivannathanp99" }}
-            target="_blank"
-            aria-label="LinkedIn"
-          >
-            <FontAwesomeIcon icon={faLinkedin} />
-          </Link>
+      <section class="social-media">
+        <div class="social-media-wrap">
+          <small class="website-rights">
+            Made by Ivan Nathan Parmenas © 2022
+          </small>
+          <div class="social-icons">
+            <a
+              class="social-icon-link instagram"
+              href="https://www.instagram.com/ivan_nathan1/"
+              target="_blank"
+              rel="noreferrer noopener"
+              aria-label="Instagram"
+            >
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+            <a
+              class="social-icon-link whatsapp"
+              href="https://wa.me/6289638303065"
+              target="_blank"
+              rel="noreferrer noopener"
+              aria-label="Whatsapp"
+            >
+              <FontAwesomeIcon icon={faWhatsapp} />
+            </a>
+            <a
+              class="social-icon-link linkedin"
+              href="https://www.linkedin.com/in/ivannathanp99"
+              target="_blank"
+              rel="noreferrer noopener"
+              aria-label="LinkedIn"
+            >
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          </div>
         </div>
-      </div>
-    </section>
-  </div>
+      </section>
+    </div>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -32,6 +32,7 @@ import FormControl from "@mui/material/FormControl";
 import { ThreeDots } from "react-loader-spinner";
 import Navbar from "../components/Navbar/Navbar";
 import Cards from "../components/Cards/Cards";
+import Footer from "../components/Footer/Footer";
 
 function Home() {
   const form = useRef();
@@ -239,41 +240,7 @@ function Home() {
           </div>
         </div>
 
-        <div className="footer-container">
-          <section class="social-media">
-            <div class="social-media-wrap">
-              <small class="website-rights">
-                Made by Ivan Nathan Parmenas © 2022
-              </small>
-              <div class="social-icons">
-                <Link
-                  class="social-icon-link instagram"
-                  to={{ pathname: "https://www.instagram.com/ivan_nathan1/" }}
-                  target="_blank"
-                  aria-label="Instagram"
-                >
-                  <FontAwesomeIcon icon={faInstagram} />
-                </Link>
-                <Link
-                  class="social-icon-link whatsapp"
-                  to={{ pathname: "https://wa.me/6289638303065" }}
-                  target="_blank"
-                  aria-label="Whatsapp"
-                >
-                  <FontAwesomeIcon icon={faWhatsapp} />
-                </Link>
-                <Link
-                  class="social-icon-link linkedin"
-                  to={{ pathname: "www.linkedin.com/in/ivannathanp99" }}
-                  target="_blank"
-                  aria-label="LinkedIn"
-                >
-                  <FontAwesomeIcon icon={faLinkedin} />
-                </Link>
-              </div>
-            </div>
-          </section>
-        </div>
+       <Footer/>
       </div>
     </>
   );
